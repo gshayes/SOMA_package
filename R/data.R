127100d632b783013bdbb87dc0fdfd4c74ebf93f
Gracie Hayes <gracejohnson2027@u.northwestern.edu>
2026-05-21 16:35:28 -0500

    data folder finished

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' @title Example Data
+#'
+#' @description A data set of effect sizes and standard errors for the meta analyses in the example.
+#'
+#' @format A data frame with 2 rows and 4 variables:
+#' \describe{
+#'   \item{MA1_1}{Data from the first study in the first meta analysis.}
+#'   \item{MA1_2}{Data from the second study in the first meta analysis.}
+#'   \item{MA2_1}{Data from the first study in the second meta analysis.}
+#'   \item{MA2_2}{Data from the second study in the second meta analysis.}
+#' }
+#' @source <https://www.github.com/gshayes/SOMA>
+"example_data"
